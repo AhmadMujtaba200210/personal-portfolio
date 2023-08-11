041ee222bc8f32f55320aec999fc629b5831efff
AhmadMujtaba200210 <cadeta6@gmail.com>
2023-08-12 01:26:22 +0500

    feat: about component done

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react';
 import Header from './components/header/Header';
 import Home  from './components/home/Home';
+import About from './components/about/About';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
 
     <main className='main'>
         <Home/>
+        <About/>
     </main>
     
   </>
