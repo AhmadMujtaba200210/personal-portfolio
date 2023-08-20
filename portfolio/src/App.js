d67b0d667ba51ffb0fa6b36749b331da02b5ed85
AhmadMujtaba200210 <cadeta6@gmail.com>
2023-08-20 13:23:29 +0500

    error: services component fixed

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -6,6 +6,7 @@ import About from './components/about/About';
 import Skills from './components/skills/Skills';
 import Qualification from './components/qualification/Qualification';
 import Quote from './components/quote/Quote';
+import Services from './components/services/Services';
 function App() {
   return (
   <>
@@ -17,6 +18,7 @@ function App() {
         <Home/>
         <About/>
         <Skills/>
+        <Services/>
         <Qualification/>
     </main>
     
